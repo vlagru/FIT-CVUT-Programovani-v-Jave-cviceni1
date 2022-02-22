f0f658c786ca7e53957bf25f4e5e21667034ad04
Vladimir Grulich <grulivla@fit.cvut.cz>
2022-02-22 15:25:31 +0100

    Add new changes

---
+++ src/org/grulivla/seminar2/SeminarNo2Balik.java
@@ -0,0 +1,34 @@
+package org.grulivla.seminar2;
+
+public class SeminarNo2Balik {
+    public static void main(String[] args) {
+    /*
+    * hluboka kopie :
+    * int a, b;
+    * a = b;
+    *
+    * enum
+    * VLASTNOSTI OOP :
+    * abstrakce : kolik pasazeru, detaily me nezajimaji, kvuli vypoctu hmotnosti
+    * dedicnost :
+    * polymorfismus : pro tridu Animal definujeme metodou int getNumberOfLegs();
+    * tridy 'cat' a 'chicken' si ji kazda rozebere po svem
+    * zapouzdreni
+    * java -cp ... [classpath] : kde se nachazi muj projekt
+    * java -version : prikazovy radek
+    * konstruktor zavola 'new'
+    * overloading konstruktoru, metod : metody se sice jmenuji stejne, ale vi, ze je to jinak, dle parametru, nebo dle
+    * --typu parametru
+    * DRY : stejne kody --- hazim do jedne metody, jednodussi kod, lepe se debuguje
+    * HYBERNATE
+    * navrhove vzory : jak stavet software, aby dopadlo dobre, lehce opravitelny a rozsirovatelny, 'builder' pattern :
+    * pro konstrukci slozitych objektu, 'singleton' etc...
+    * hashing, rozptilovani :
+    * 
+     */
+
+
+
+    }
+
+}
