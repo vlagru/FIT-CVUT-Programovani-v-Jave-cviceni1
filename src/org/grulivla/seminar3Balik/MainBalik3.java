04b3178916021aa68a78a85b896a2ef6cb2ad22e
Vladimir Grulich <grulivla@fit.cvut.cz>
2022-03-01 15:59:33 +0100

    Add seminar3Balik

---
+++ src/org/grulivla/seminar3Balik/MainBalik3.java
@@ -0,0 +1,15 @@
+package org.grulivla.seminar3Balik;
+
+public class MainBalik3 {
+    public static void main(String[] args) {
+        /*
+        * pole : referencni datovy typ, .length, neumi se natahovat, proto ho nikdo nepouziva
+        * rozhrani : interface, programatorske rozhrani, funkcni rozhrani : predikat (Object, true nebo false)
+        * --- mohou tam byt konstanty, ne nestaticke, tedy nema atributy, pouze finalni atributy
+        * --- mohou tam byt metody, staticke, default a abstraktni metody
+        * --- defaultni metoda : poskytneme implmentaci, pokud ji neprepiseme
+        * pormenny pocet parametru v metodach se vyznacuje '...'
+        *
+        */
+    }
+}
